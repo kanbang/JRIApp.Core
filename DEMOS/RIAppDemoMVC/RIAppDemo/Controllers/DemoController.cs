2061c012ae1d7b8f270546b54bccb6f6e6ab32af
Maxim T <lptscsi@gmail.com>
2019-02-26 03:32:12 +0300

    Url update

--- DEMOS/RIAppDemoMVC/RIAppDemo/Controllers/DemoController.cs
+++ DEMOS/RIAppDemoMVC/RIAppDemo/Controllers/DemoController.cs
@@ -13,7 +13,6 @@ namespace RIAppDemo.Controllers
 
         public ActionResult DataGridDemo()
         {
-            ViewData["BaseURL"] = $"{this.Request.PathBase}";
             return View();
         }
 
