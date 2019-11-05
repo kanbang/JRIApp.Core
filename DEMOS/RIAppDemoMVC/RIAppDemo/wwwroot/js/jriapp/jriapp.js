486f877346f814809151d30100438f34d68c7502
Maxim V. Tsapov <lptscsi@gmail.com>
2019-11-05 15:26:56 +0300

    Include ServerCalculated into refresh results

--- DEMOS/RIAppDemoMVC/RIAppDemo/wwwroot/js/jriapp/jriapp.js
+++ DEMOS/RIAppDemoMVC/RIAppDemo/wwwroot/js/jriapp/jriapp.js
@@ -4825,6 +4825,6 @@ define("jriapp", ["require", "exports", "jriapp/bootstrap", "jriapp_shared", "jr
     exports.BaseCommand = mvvm_1.BaseCommand;
     exports.Command = mvvm_1.Command;
     exports.Application = app_1.Application;
-    exports.VERSION = "2.25.11";
+    exports.VERSION = "2.25.12";
     bootstrap_7.Bootstrap._initFramework();
 });
--- FRAMEWORK/CLIENT/jriapp/jriapp.ts
+++ FRAMEWORK/CLIENT/jriapp/jriapp.ts
@@ -25,6 +25,6 @@ export { ViewModel, BaseCommand, Command, ICommand } from "./jriapp/mvvm";
 
 export { Application } from "./jriapp/app";
 
-export const VERSION = "2.25.11";
+export const VERSION = "2.25.12";
 
 Bootstrap._initFramework();
